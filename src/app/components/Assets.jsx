071cbb6cfcb11f18085a0daa412d5b2b6b93b7b7
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-07 21:08:35 +0530

    ADD : LAYOUT

---
+++ src/app/components/Assets.jsx
@@ -0,0 +1,2 @@
+export const logo = "https://res.cloudinary.com/duwddcqzi/image/upload/v1736246482/newsly-high-resolution-logo_1_deopjy.png"
+export const logo_dark = "https://res.cloudinary.com/duwddcqzi/image/upload/v1736263771/newsly-high-resolution-logo_2_ia33mb.png"
---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { logo } from './Assets'
+
+
+function Navbar() {
+  return (
+    <div>
+        <>
+        {/* const [Active, setActive] = useState(false);
+        function onclickmenu (){
+            setActive(!Active)
+        } */}
+        <div className="nav flex h-[70px] flex-row items-center shadow-[0px_1px_8px_grey] w-full fixed z-[2] bg-[white] overflow-hidden">
+          <div className="navigation">
+          </div>
+          <div className="logo flex h-[25px] pl-[45%]">
+            <img
+                src={logo}
+              alt="no logo"
+              className=' h-36 my-[-55px] ml-[-20px]'
+            />
+          </div>
+        </div>
+      </>
+      
+    </div>
+  )
+}
+
+export default Navbar
