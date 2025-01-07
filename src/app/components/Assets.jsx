d6b210af37c1091348ec35ec17aa95867f5f0a2e
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-07 23:47:03 +0530

    7/01/25

--- src/app/components/Assets.jsx
+++ src/app/components/Assets.jsx
@@ -1,2 +1,2 @@
 export const logo = "https://res.cloudinary.com/duwddcqzi/image/upload/v1736246482/newsly-high-resolution-logo_1_deopjy.png"
-export const logo_dark = "https://res.cloudinary.com/duwddcqzi/image/upload/v1736263771/newsly-high-resolution-logo_2_ia33mb.png"
+export const logo_dark = "https://res.cloudinary.com/duwddcqzi/image/upload/v1736272738/newsly-high-resolution-logo-transparent_jkpyk2.png"
--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -7,18 +7,17 @@ function Footer() {
     <div className="foot bg-[#323232] text-[white] h-[140px] w-full flex py-5">
         <div className="foot1 flex flex-row px-[14%] py-0">
         <div className="f1 flex flex-col">
-            <img src={logo_dark} alt="no" className='img h-20 w-[170px] bg-blend-screen mb-[-30px] mt-[-20px]'/><br />
+            <img src={logo_dark} alt="no" className='img h-10 w-[170px] bg-blend-screen mb-[-10px] '/><br />
             <div className="b flex justify-content: end"><b className='text-[15px] opacity-[65%] pr-2.5'>News.ly</b></div><div className="txt text-[13px] text-[gray] flex pr-2.5 justify-content: end">Pte.Ltd.&copy;COPYRIGHT 2024</div>
         </div>
         <div className="f2 pl-5">
-            <a href="/" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/contact_icon.png" alt="no" className="img2 h-[75px] w-[130px] -mt-2.5 pb-[25px]" /></a>
-            <div className="txt2 text-[13px] -mt-2.5"><a href="https://inshorts.com/tnc" className='no-underline text-[white]'>Terms&Conditions </a><br />
-            <a href="https://inshorts.com/tnc" className='no-underline text-[white]'>Privacy Policies</a></div>
+            <a href="/" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/contact_icon.png" alt="no" className="img2 h-[75px] w-[170px] -mt-2.5 pb-[25px]" /></a>
+            
         </div>
         <div className="f3 flex flex-row pl-[125%] align-items: end">
-            <div className="fb"><a href="https://www.facebook.com/news" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/facebook.png" alt="no " className='ics h-[30px] w-[30px] opacity-[80%] m-2' /></a></div>
-            <div className="twi"><a href="https://twitter.com/ShadabAnwa27994" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/twitter.png" alt="no " className='ics h-[30px] w-[30px] opacity-[80%] m-2' /></a></div>
-            <div className="link"><a href="https://www.linkedin.com/in/shadab-anwar/" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/linkedin.png" alt="no" className='ics h-[30px] w-[30px] opacity-[80%] m-2'/></a></div>
+            <div className="fb"><a href="https://www.facebook.com/news" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/facebook.png" alt="no " className='ics h-[50px] w-[50px] opacity-[80%] m-2 p-2' /></a></div>
+            <div className="twi"><a href="https://twitter.com/ShadabAnwa27994" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/twitter.png" alt="no " className='ics h-[50px] w-[50px] opacity-[80%] m-2 p-2' /></a></div>
+            <div className="link"><a href="https://www.linkedin.com/in/shadab-anwar/" className='no-underline text-[white]'><img src="https://assets.inshorts.com/website_assets/images/linkedin.png" alt="no" className='ics h-[50px] w-[50px] opacity-[80%] m-2 p-2'/></a></div>
         </div>
         </div>
         </div>
