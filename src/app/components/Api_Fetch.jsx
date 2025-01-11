9e10f5e8fc01cde7ffc32dbabc9649aa176c8671
Shadab Anwar <shadabanwar1810@gmail.com>
2025-01-11 17:44:29 +0530

    filtered news added

--- src/app/components/Api_Fetch.jsx
+++ src/app/components/Api_Fetch.jsx
@@ -11,14 +11,21 @@ function Api_Fetch() {
         const fetching = async () => {
             try {
                 const Api_key = process.env.NEXT_PUBLIC_API_KEY 
-                const res = await fetch(`https://newsapi.org/v2/everything?q=tesla&from=2024-12-10&sortBy=publishedAt&apiKey=${Api_key}`);
+                const res = await fetch(`https://newsapi.org/v2/everything?q=apple&from=2025-01-10&to=2025-01-10&sortBy=popularity&apiKey=${Api_key}`);
                 if (!res.ok) {
                     console.log("Not fetched the data");
                 }
                 const data = await res.json()
                 // console.log(data);
-                // console.log(data.articles);
-                setNews(data.articles);
+                console.log(data.articles);
+                const Non_Filtered_News = data.articles;
+                console.log(Non_Filtered_News)
+
+                const filteredNews = Non_Filtered_News.filter((item) =>{
+                     return item.title !== "[Removed]"
+                    }); 
+                console.log(filteredNews);
+                setNews(filteredNews);
             } catch (error) {
                 throw new Error ("Failed to fetch the data")
             }   
@@ -29,6 +36,8 @@ function Api_Fetch() {
   return (
     <div>{
         news.map((Element , Index)=>{
+
+            
                 return <NewsCard key={Index} author={Element.author} source={Element.source.name} title={Element.title} desc={Element.description} content={Element.content} pub={Element.publishedAt} url={Element.url} Image={Element.urlToImage} />
         })
 }</div>
